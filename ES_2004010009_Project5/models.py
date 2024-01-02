d061589bde5cdbfde544e473dbb3cfa8e51a0cd3
Emre Savaş <emre_savas28@hotmail.com>
2024-01-02 14:19:51 +0300

    Finish first question.

--- ES_2004010009_Project5/models.py
+++ ES_2004010009_Project5/models.py
@@ -26,7 +26,7 @@ class PerceptronModel(object):
             x: a node with shape (1 x dimensions)
         Returns: a node containing a single number (the score)
         """
-        "*** YOUR CODE HERE ***"
+        return nn.DotProduct(self.w, x)
 
     def get_prediction(self, x):
         """
@@ -34,13 +34,21 @@ class PerceptronModel(object):
 
         Returns: 1 or -1
         """
-        "*** YOUR CODE HERE ***"
+
+        return 1 if nn.as_scalar(self.run(x)) >= 0 else -1
 
     def train(self, dataset):
         """
         Train the perceptron until convergence.
         """
-        "*** YOUR CODE HERE ***"
+        while True:
+            flag = True
+            for x, y in dataset.iterate_once(1):
+                if self.get_prediction(x) != nn.as_scalar(y):
+                    flag = False
+                    self.w.update(x, nn.as_scalar(y))
+            if flag:
+                break
 
 class RegressionModel(object):
     """
